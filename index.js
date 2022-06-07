23641196860a477acc76782c6300a7e83d153394
BuildTools <unconfigured@null.spigotmc.org>
2022-06-07 19:42:34 -0400

    good

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ canvas.height = window.innerHeight
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d")
-
-let car = new Car(100, 100, 30, 50)
+const road = new Road(canvas.width/2, canvas.width*0.9)
+let car = new Car(road.getLaneCenter(1), 100, 30, 50)
 car.draw(ctx)
 
 function animate() {
     car.update()
     canvas.height = window.innerHeight
+    road.draw(ctx)
     car.draw(ctx)
     requestAnimationFrame(animate)
 }
