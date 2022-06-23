038d911766365c048d6f676ab0ebbdd38766712d
BuildTools <unconfigured@null.spigotmc.org>
2022-06-23 17:01:03 -0400

    lol

--- network.js
+++ network.js
@@ -37,7 +37,7 @@ class Level {
         return level.outputs
     }
 
-    constructor(inputCount, outputCount) {
+    constructor(inputCount, outputCount) { //input is the input (basically previous neuron layer)
         this.inputs = new Array(inputCount)
         this.outputs = new Array(outputCount)
         this.biases = new Array(outputCount)
