c0ee70bbca3782d24f3af0301de8c956b79dabc3
BuildTools <unconfigured@null.spigotmc.org>
2022-06-27 17:36:07 -0400

    nice

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-let canvas = document.getElementById("carCanvas")
-canvas.height = window.innerHeight
-canvas.width = 200;
+let carCanvas = document.getElementById("carCanvas")
+carCanvas.height = window.innerHeight
+carCanvas.width = 200;
 
-const ctx = canvas.getContext("2d")
-const road = new Road(canvas.width/2, canvas.width*0.9)
+let networkCanvas = document.getElementById("networkCanvas")
+networkCanvas.height = window.innerHeight
+
+console.log(window.innerWidth)
+
+const carCtx = carCanvas.getContext("2d")
+const networkCtx = networkCanvas.getContext("2d")
+
+const road = new Road(carCanvas.width/2, carCanvas.width*0.9)
 let car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI", 5)
 let traffic = [
     new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY")
@@ -17,19 +24,23 @@ function animate() {
 
     car.update(road.borders, traffic)
 
-    canvas.height = window.innerHeight
+    carCanvas.height = window.innerHeight
+    networkCanvas.height = window.innerHeight
+    networkCanvas.width = window.innerWidth - carCanvas.width - 5;
 
-    ctx.save()
-    ctx.translate(0, -car.y + canvas.height/1.2)
-    road.draw(ctx)
+    carCtx.save()
+    carCtx.translate(0, -car.y + carCanvas.height/1.2)
+    road.draw(carCtx)
 
     for (let i = 0; i<traffic.length; i++) {
-        traffic[i].draw(ctx)
+        traffic[i].draw(carCtx)
     }
 
-    car.draw(ctx, "red")
+    car.draw(carCtx, "red")
 
-    ctx.restore()
+    carCtx.restore()
+
+    Visualizer.drawNetwork(networkCtx, car.brain)
     requestAnimationFrame(animate)
 }
 
